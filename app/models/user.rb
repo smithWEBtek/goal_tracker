cc8e9bf6fe562c4ad75e095fcc19a9c66f2f06c2
sunyounghwang <sunyounghwang13@gmail.com>
2018-12-19 15:39:02 -0500

    implement belongs_to/has_many for users and goals

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  has_many :goals, dependent: :destroy
+
   validates :username, presence: true
   validates :username, uniqueness: true
+
   has_secure_password
 end
