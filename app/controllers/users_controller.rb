5764ca860a47c0995d35c15f233eaf7697aa7b53
sunyounghwang <sunyounghwang13@gmail.com>
2018-12-19 11:29:39 -0500

    build create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,22 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+
+    if user.save
+      session[:user_id] = user.id
+      redirect_to user_url(user)
+    else
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
