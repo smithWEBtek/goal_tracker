9279ef2ce10359ef39a7fdbf75a58a92e5ded2b9
sunyounghwang <sunyounghwang13@gmail.com>
2018-12-19 16:55:03 -0500

    build goals#new and goals#create and goal_params method

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,10 +1,27 @@
 class GoalsController < ApplicationController
   def new
+    @goal = current_user.goals.new
+  end
+
+  def create
+    @goal = current_user.goals.build(goal_params)
+
+    if @goal.save
+      redirect_to goal_url(@goal)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def show
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(:name, :summary)
   end
 end
