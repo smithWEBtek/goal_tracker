e6abb37c27d35056756db6db5b384136cd43eda5
sunyounghwang <sunyounghwang13@gmail.com>
2018-12-19 10:50:58 -0500

    map login and logout routes for sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
   root 'welcome#index'
-  
+
   resources :users, only: [:new, :create, :show]
 end
