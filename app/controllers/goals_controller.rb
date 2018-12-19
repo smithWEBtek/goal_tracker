6da1695b7fa45ed77fc297b0af39841ac0f45207
sunyounghwang <sunyounghwang13@gmail.com>
2018-12-19 15:42:58 -0500

    generate goals controller with #new #edit #show

---
+++ app/controllers/goals_controller.rb
@@ -0,0 +1,10 @@
+class GoalsController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'goals/new'
+  get 'goals/edit'
+  get 'goals/show'
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
