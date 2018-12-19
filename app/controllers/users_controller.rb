b8c313aef2aacf189b852e908668c1a2b0c42d47
sunyounghwang <sunyounghwang13@gmail.com>
2018-12-19 11:48:24 -0500

    change user to instance variable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,11 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
+    @user = User.new(user_params)
 
-    if user.save
-      session[:user_id] = user.id
-      redirect_to user_url(user)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_url(@user)
     else
       render :new
     end
