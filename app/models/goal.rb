44c25e79691d12baf64bedda074a6e58da08b28a
sunyounghwang <sunyounghwang13@gmail.com>
2018-12-20 13:39:50 -0500

    create completed and incomplete goal scopes

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,4 +2,7 @@ class Goal < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true
+
+  scope :completed, -> { where(completed: true) }
+  scope :incomplete, -> { where(completed: false) }
 end
