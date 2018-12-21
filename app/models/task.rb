4b3026b7f7f026d63f1f013d1c3ac29e627d381e
sunyounghwang <sunyounghwang13@gmail.com>
2018-12-21 11:24:15 -0500

    add name validation and in/complete scopes to task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :goal
+
+  validates :name, presence: true
+
+  scope :completed, -> { where(completed: true) }
+  scope :incomplete, -> { where(completed: false) }
 end
