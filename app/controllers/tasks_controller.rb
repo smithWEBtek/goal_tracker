994b5a36a702fdc320b7ae90f71cdbdc401baed0
sunyounghwang <sunyounghwang13@gmail.com>
2018-12-21 11:05:57 -0500

    create tasks#new and tasks#create and task_params method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,23 @@
 class TasksController < ApplicationController
   before_action -> { set_goal(params[:goal_id]) }, only: [:new, :create]
+
+  def new
+    @task = @goal.tasks.new
+  end
+
+  def create
+    @task = @goal.tasks.build(task_params)
+
+    if @task.save
+      redirect_to goal_url(@goal)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name)
+  end
 end
