24db152be222e15e8dec4dd273b51509f288a979
sunyounghwang <sunyounghwang13@gmail.com>
2018-12-22 18:50:26 -0500

    build category_name setter and getter for goal model

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -15,4 +15,12 @@ class Goal < ApplicationRecord
   def completed_tasks
     tasks.completed
   end
+
+  def category_name=(name)
+    self.category = Category.find_or_create_by(name: name)
+  end
+
+  def category_name
+    self.category ? self.category.name : nil
+  end
 end
