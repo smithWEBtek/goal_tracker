2ed520f7e91f2b75259b025ea8c44f598132754a
sunyounghwang <sunyounghwang13@gmail.com>
2018-12-22 14:47:12 -0500

    create goals helpers, partials, and refactor goal show page

--- app/helpers/goals_helper.rb
+++ app/helpers/goals_helper.rb
@@ -1,2 +1,19 @@
 module GoalsHelper
+  def incomplete_link_if_completed(goal)
+    link_to("Reopen", incomplete_goal_path(goal), method: :patch) if goal.completed
+  end
+
+  def complete_link_if_tasks_completed(goal)
+    if !goal.completed && goal.incomplete_tasks.none?
+      link_to("Complete", complete_goal_path(goal), method: :patch)
+    end
+  end
+
+  def goal_status_action_link(goal)
+    incomplete_link_if_completed(goal) || complete_link_if_tasks_completed(goal)
+  end
+
+  def render_task_links_if_incomplete(goal)
+    render partial: "new_edit_task_links", locals: {goal: goal} unless goal.completed
+  end
 end
