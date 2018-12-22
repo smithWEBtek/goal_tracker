917fbba6d811f64ba8a8d230efecc880899aca79
sunyounghwang <sunyounghwang13@gmail.com>
2018-12-22 18:47:18 -0500

    add name validations to category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
   has_many :goals
   has_many :users, through: :goals
+
+  validates :name, presence: true
+  validates :name, uniqueness: { case_sensitive: false }
 end
