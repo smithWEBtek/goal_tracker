dc147820489374a67dfdd7db836c355e17219495
sunyounghwang <sunyounghwang13@gmail.com>
2018-12-22 19:00:55 -0500

    add unique index to categories name

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_22_211228) do
+ActiveRecord::Schema.define(version: 2018_12_22_235658) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "goals", force: :cascade do |t|
