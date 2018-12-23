fd59ef036e1e865c41f0f5d6c8a2e5e61e6b9bef
sunyounghwang <sunyounghwang13@gmail.com>
2018-12-23 11:12:57 -0500

    build user creation and login functionality for omni_auth#facebook

--- app/controllers/omni_auth_controller.rb
+++ app/controllers/omni_auth_controller.rb
@@ -2,5 +2,14 @@ class OmniAuthController < ApplicationController
   skip_before_action :require_login, only: [:facebook]
 
   def facebook
+    auth = request.env['omniauth.auth']
+
+    user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.username = auth['info']['name']
+      u.password = SecureRandom.hex
+    end
+
+    session[:user_id] = user.id
+    redirect_to user_url(user)
   end
 end
