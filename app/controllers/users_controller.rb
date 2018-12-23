72f1d2ecf8f65bfeaaa88f2b930b28a56e7457da
sunyounghwang <sunyounghwang13@gmail.com>
2018-12-23 09:49:30 -0500

    redirect to user show page if visiting other users pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,10 @@ class UsersController < ApplicationController
   end
 
   def show
-    flash.now.alert = "Sorry, you can only view your own profile" unless current_user == User.find_by(id: params[:id])
+    unless current_user == User.find_by(id: params[:id])
+      flash.alert = "Sorry, you can only view your own profile"
+      redirect_to user_url(current_user)
+    end
     @incomplete_goals = current_user.incomplete_goals
     @completed_goals = current_user.completed_goals
   end
