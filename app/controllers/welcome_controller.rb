f9abfac0bd85bfd94881d60aad0cbd422cab5a92
sunyounghwang <sunyounghwang13@gmail.com>
2018-12-23 13:45:24 -0500

    add intro and list most recently created goals on homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
   skip_before_action :require_login, only: [:index]
 
   def index
+    @recently_created_goals = Goal.most_recently_created(5)
   end
 end
