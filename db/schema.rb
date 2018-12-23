e2dc8c88665c3409587d90339c27c8bbb440b428
sunyounghwang <sunyounghwang13@gmail.com>
2018-12-23 11:10:02 -0500

    add uid attribute to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_22_235658) do
+ActiveRecord::Schema.define(version: 2018_12_23_152948) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2018_12_22_235658) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
   end
 
 end
