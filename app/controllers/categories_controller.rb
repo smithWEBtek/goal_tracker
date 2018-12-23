de6214a5428de84a1fd92217154d5a24a00dc434
sunyounghwang <sunyounghwang13@gmail.com>
2018-12-22 19:50:02 -0500

    use most_recently_created scope in categories#show and show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
   def show
+    @category = Category.find_by(id: params[:id])
+    @recently_created_goals = @category.goals.most_recently_created(10)
   end
 end
