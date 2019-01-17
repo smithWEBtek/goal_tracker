972ceb16d685430d7350f4d5cdb2ba319f7d8434
sunyounghwang <sunyounghwang13@gmail.com>
2019-01-17 10:35:36 -0500

    change goal next link functionality to js model format

--- app/assets/javascripts/goals.js
+++ app/assets/javascripts/goals.js
@@ -1,15 +1,31 @@
-$(function() {
-  $(document).on("click", ".next", (e) => {
-    e.preventDefault();
-    const $target = $(e.currentTarget);
+function Goal(goal) {
+  this.id = goal.id;
+  this.show = goal.show;
+}
 
-    $.getJSON("/goals/" + $target.data("id"), goals => {
-      const currIndex = goals.findIndex(goal => { return goal.id === $target.data("id") })
-      const nextIndex = (currIndex + 1) % goals.length;
-      const nextGoal = goals[nextIndex];
-      $("#show").empty().html(nextGoal["show"]);
-      $target.data("id", nextGoal["id"]);
-      history.pushState(null, null, "/goals/" + nextGoal["id"]);
-    });
+Goal.addClickListener = function() {
+  $(document).on("click", ".next", function(e) {
+    e.preventDefault();
+    Goal.clickEvent(this);
   });
-});
+}
+
+Goal.clickEvent = function(target) {
+  $target = $(target);
+  $.getJSON("/goals/" + $target.data("id"), goals => {
+    const currIndex = goals.findIndex(goal => goal.id === $target.data("id"))
+    const nextIndex = (currIndex + 1) % goals.length;
+    const nextGoal = new Goal(goals[nextIndex]);
+    nextGoal.display();
+    $target.data("id", nextGoal.id);
+    history.pushState(null, null, "/goals/" + nextGoal.id);
+  });
+}
+
+Goal.prototype.display = function() {
+  $("#show").empty().html(this.show);
+}
+
+$(function() {
+  Goal.addClickListener();
+})
