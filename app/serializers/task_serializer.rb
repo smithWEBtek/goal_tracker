bdd87681ff0cc380af140df6148b93c930eecf9e
sunyounghwang <sunyounghwang13@gmail.com>
2019-01-18 12:45:29 -0500

    render and include error messages partial in task serializer

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,7 +1,11 @@
 class TaskSerializer < ActiveModel::Serializer
-  attributes :id, :incomplete_html
+  attributes :id, :incomplete_html, :errors
 
   def incomplete_html
     TasksController.render(partial: "incomplete_task", locals: { task: object }, layout: false).squish if object.persisted?
+  end
+
+  def errors
+    TasksController.render(partial: "error_messages", locals: { object: object }, layout: false).squish
   end
 end
