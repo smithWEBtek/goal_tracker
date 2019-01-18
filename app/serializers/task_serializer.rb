e8893f7b4881a0fbc216b031421d2c3d5c0077ae
sunyounghwang <sunyounghwang13@gmail.com>
2019-01-18 12:44:11 -0500

    render and include incomplete task partial in task serializer

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,3 +1,7 @@
 class TaskSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :incomplete_html
+
+  def incomplete_html
+    TasksController.render(partial: "incomplete_task", locals: { task: object }, layout: false).squish if object.persisted?
+  end
 end
